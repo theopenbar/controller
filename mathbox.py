2ac23bcc208c3e381b933de60ec0f0488c08f02e
Tyler <tylerhorwat@gmail.com>
2017-03-05 02:41:12 +0000

    Trying auto start and keyboard

---
+++ mathbox.py
@@ -0,0 +1,3 @@
+import os
+os.system("matchbox-keyboard")
+quit()
---
+++ startcontroller.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+sudo python /home/pi/controller/matchbox.py
+
+sudo python /home/pi/controller/tob_daemon.py
+
+sudo matchbox-keyboard -d
+
