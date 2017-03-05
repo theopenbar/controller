0f1d7bd6a3dc9810eb8da90c7a23d38fa51be221
thoughtfull <tylerhorwat@gmail.com>
2017-03-05 23:19:04 +0000

    Remove Autostart Script

--- mathbox.py
+++
@@ -1,3 +0,0 @@
-import os
-os.system("matchbox-keyboard")
-quit()
--- startcontroller.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-sudo python /home/pi/controller/matchbox.py
-
-sudo python /home/pi/controller/tob_daemon.py
-
-sudo matchbox-keyboard -d
-
